a56910f46b70b4d357f6aaf415f0086de4a1c609
Alex <chugun777akk@mail.ru>
2022-04-03 10:43:29 +0700

    task6 is done

---
+++ HW6Task6/Program.cs
@@ -0,0 +1,65 @@
+﻿// В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента не
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = 0;
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} {" "}");
+        }
+        Console.WriteLine();
+    }
+}
+void PartialReplacement(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                Console.Write("Enter number: ");
+                int Element = Convert.ToInt32(Console.ReadLine());
+                arr[i, j] = Element;
+
+            }
+        }
+    }
+}
+void AnalisArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] > 0)
+            {
+                Console.Write("Indices of number which entered user  " + i + " " + j + "\n");
+            }
+        }
+    }
+}
+Console.Write("Enter number of strings: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+PartialReplacement(array);
+Console.WriteLine();
+PrintArray(array);
+AnalisArray(array);
+
+
+

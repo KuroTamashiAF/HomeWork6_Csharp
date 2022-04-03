040bf6281d860e8cb97084beac4c6ba72b4a443c
Alex <chugun777akk@mail.ru>
2022-04-03 09:45:57 +0700

    task2 is done

---
+++ HW6Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} {" "}");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Enter number of strings: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);

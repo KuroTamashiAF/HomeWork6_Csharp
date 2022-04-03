35f2bac412ec8adad80d31118088def4bd16d009
Alex <chugun777akk@mail.ru>
2022-04-03 09:48:46 +0700

    task3 is done

---
+++ HW6Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0,101);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} {" "}");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Enter number of strings: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
